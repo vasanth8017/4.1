ad4f2e0cc98a76dbbaeed9eece9034a562155fcc
vasanth8017 <91736638+vasanth8017@users.noreply.github.com>
2022-02-09 17:16:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function renderPosts(boston, container) {
+    const people = boston.data;
+    people.sort((a,b)=>{return a[11]-b[11]})
+    people.splice(5,length)
+    const len = boston.data.length;
+    var topSalaryPeople=[];
+    let html = '';
+    
+    
+    
+    for (let i = 0; i < len; i++) {
+   if(people[i][11]>=100000){
+
+  
+      html +=
+        '<li class="post">' + '<h2>' + people[i][8] + '</h2>' + '<h3>' + people[i][11] + '</h3>';
+       
+    }
+    }
+    
+
+    for (let i = 0; i < len; i++) {
+      if(people[i][11]<=150000){
+
+
+        html +=
+        '<li class="post">' + '<h2>' + people[i][8] + '</h2>' + '<h3>' + people[i][11] + '</h3>';
+       
+      }
+    }
+     
+    
+         // TODO: add code to display the html variable inside a ul element with id="data"
+    // Hint: you can use the container parameter's innerHTML property to insert Html tags
+    container.innerHTML = '<ul id = "data">' + html + '</ul>';
+    
+  }
+  renderPosts(boston, document.getElementById('container'));
